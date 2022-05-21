a0723247bd4bde07c8fd5d64f90f02eff6edb5e1
NikitaVirachev <guardsolder@gmail.com>
2022-05-21 21:47:07 +0300

    fix: brought code in task 04-copy-directory to instructions with eslint

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, () => {
   fs.readdir(path.join(__dirname, 'files'), (err, files) => {
     for (const file of files) {
       fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), () => {});
     }
-  })
+  });
 });

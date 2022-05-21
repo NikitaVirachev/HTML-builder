8ebb44bec61c542dc2184e63f2d4b7daff62c18b
NikitaVirachev <guardsolder@gmail.com>
2022-05-21 21:41:24 +0300

    complete task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const output = fs.createWriteStream( path.join(__dirname, 'project-dist', 'bundle.css') );
+
+fs.readdir( path.join(__dirname, 'styles'), {withFileTypes: true}, (err, files) => {
+  for (const file of files) {
+    if (file.isFile() && path.extname(file.name) === '.css') {
+      let readableStream = fs.createReadStream( path.join(__dirname, 'styles', file.name) );
+      readableStream.on('data', chunk => output.write(chunk));
+    }
+  }
+});

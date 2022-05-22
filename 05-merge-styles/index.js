f1587f2842cc7b07f9fbc8b2bdff2d8241427006
NikitaVirachev <guardsolder@gmail.com>
2022-05-22 21:22:21 +0300

    fix: add padding after chunk in task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -7,7 +7,7 @@ fs.readdir( path.join(__dirname, 'styles'), {withFileTypes: true}, (err, files) 
   for (const file of files) {
     if (file.isFile() && path.extname(file.name) === '.css') {
       let readableStream = fs.createReadStream( path.join(__dirname, 'styles', file.name) );
-      readableStream.on('data', chunk => output.write(chunk));
+      readableStream.on('data', chunk => output.write(`${chunk}\n`));
     }
   }
 });

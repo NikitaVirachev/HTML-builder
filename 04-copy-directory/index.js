e257a3299eba5f27ac6483744f1af3b839653416
NikitaVirachev <guardsolder@gmail.com>
2022-05-22 22:03:49 +0300

    fix: non-existent files are now delete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,7 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, () => {
+  fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+    if (err) console.log(err);
+    for (const file of files) {
+      fs.unlink(path.join(__dirname, 'files-copy', file), () => {});
+    }
+  });
   fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err) console.log(err);
     for (const file of files) {
       fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), () => {});
     }
